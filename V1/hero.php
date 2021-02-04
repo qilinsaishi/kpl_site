ec4f5c9ec85421783f4db98c9070d31aa9b9465c
xiaodong chen <cxd032404@hotmail.com>
2021-02-04 17:20:24 +0800

    merge

--- V1/hero.php
+++ V1/hero.php
@@ -11,22 +11,11 @@
      "defaultConfig"=>["keys"=>["contact","sitemap"],"fields"=>["name","key","value"]],
      "links"=>["game"=>$config['game'],"page"=>1,"page_size"=>6,"site_id"=>$config['site_id']],
      "totalPlayerList"=>["game"=>$config['game'],"page"=>1,"page_size"=>3,"source"=>"cpseo","fields"=>'player_id,player_name,logo',"rand"=>1,"cacheWith"=>"currentPage"],
-     "keywordMapList"=>["fields"=>"content_id","game"=>$config['game'],"source_type"=>"hero","source_id"=>$hero_id,"page_size"=>100,"content_type"=>"information"],
+     "keywordMapList"=>["fields"=>"content_id","game"=>$config['game'],"source_type"=>"hero","source_id"=>$hero_id,"page_size"=>100,"content_type"=>"information","list"=>["page_size"=>13,"type"=>4,"fields"=>"id,title,logo,type,create_time"]],
      "currentPage"=>["name"=>"hero","id"=>$hero_id,"site_id"=>$config['site_id']]
  ];
  $return = curl_post($config['api_get'],json_encode($data),1);
- if(count($return["keywordMapList"]['data'])>0)
- {
-     $data2 = [
-         "informationList"=>["ids"=>array_column($return["keywordMapList"]['data'],"content_id"),"page_size"=>13,"type"=>4,"fields"=>"id,title,logo,type"]
-     ];
-     $return2 = curl_post($config['api_get'],json_encode($data2),1);
-     $connectedInformationList = $return2["informationList"]["data"];
- }
- else
- {
-     $connectedInformationList = [];
- }
+ $connectedInformationList = $return["keywordMapList"]["data"];
  ?>
 <head>
 <meta charset="UTF-8" />
