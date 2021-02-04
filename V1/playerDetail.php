33d0092b18eef923e19f7434a69dc79217a4dd2c
xiaodong chen <cxd032404@hotmail.com>
2021-02-04 17:18:44 +0800

    merge

--- V1/playerDetail.php
+++ V1/playerDetail.php
@@ -8,22 +8,12 @@
      "totalTeamList"=>["page"=>1,"page_size"=>12,"game"=>$config['game'],"source"=>"cpseo","fields"=>'team_id,team_name,logo,team_history',"rand"=>1,"cacheWith"=>"currentPage"],
      "links"=>["game"=>$config['game'],"page"=>1,"page_size"=>6,"site_id"=>$config['site_id']],
      "informationList"=>["game"=>$config['game'],"page"=>1,"page_size"=>6,"type"=>"1,2,3,5"],
-     "keywordMapList"=>["fields"=>"content_id","game"=>$config['game'],"source_type"=>"player","source_id"=>$player_id,"page_size"=>100,"content_type"=>"information"],
+     "keywordMapList"=>["fields"=>"content_id","game"=>$config['game'],"source_type"=>"player","source_id"=>$player_id,"page_size"=>100,"content_type"=>"information","list"=>["page_size"=>8,"fields"=>"id,title,create_time"]],
      "currentPage"=>["name"=>"player","id"=>$player_id,"site_id"=>$config['site_id']]
  ];
+
  $return = curl_post($config['api_get'],json_encode($data),1);
- if(count($return["keywordMapList"]['data'])>0)
- {
-     $data2 = [
-         "informationList"=>["ids"=>array_column($return["keywordMapList"]['data'],"content_id"),"page_size"=>8,"fields"=>"id,title"]
-     ];
-     $return2 = curl_post($config['api_get'],json_encode($data2),1);
-     $connectedInformationList = $return2["informationList"]["data"];
- }
- else
- {
-     $connectedInformationList = [];
- }
+ $connectedInformationList = $return["keywordMapList"]["data"];
  $data3 = [
      "teamMateList"=>["dataType"=>"totalPlayerList","game"=>$config['game'],"page"=>1,"page_size"=>6,"source"=>"cpseo","fields"=>'player_id,player_name,logo,position',"team_id"=>$return['totalPlayerInfo']['data']['team_id']],
  ];
@@ -67,7 +57,8 @@
           <div class="x_m"><?php echo $return['totalPlayerInfo']['data']['player_name'];?></div>
           <div class="j_j">中文名：<?php echo $return['totalPlayerInfo']['data']['cn_name'];?><br>
           英文名：<?php echo $return['totalPlayerInfo']['data']['en_name'];?><br>
-          游戏位置：<?php echo $return['totalPlayerInfo']['data']['position'];?><br>
+              所属战队：<a href = '<?php echo $config['site_url']; ?>/teamdetail/<?php echo $return['totalPlayerInfo']['data']['teamInfo']['team_id'];?>'><?php echo $return['totalPlayerInfo']['data']['teamInfo']['team_name'];?></a><br>
+              游戏位置：<?php echo $return['totalPlayerInfo']['data']['position'];?><br>
           ID：<?php echo $return['totalPlayerInfo']['data']['player_name'];?><br>
           个人简介：<?php echo $return['totalPlayerInfo']['data']['description'];?></div>
         </div>
--- V1/teamDetail.php
+++ V1/teamDetail.php
@@ -8,28 +8,24 @@ $data = [
     "totalTeamList"=>["page"=>1,"page_size"=>12,"game"=>$config['game'],"source"=>"cpseo","fields"=>'team_id,team_name,logo,team_history',"rand"=>1,"cacheWith"=>"currentPage"],
     "defaultConfig"=>["keys"=>["contact","sitemap","default_player_img"],"fields"=>["name","key","value"]],
     "links"=>["game"=>$config['game'],"page"=>1,"page_size"=>6,"site_id"=>$config['site_id']],
-    "keywordMapList"=>["fields"=>"content_id","source_type"=>"team","source_id"=>$team_id,"page_size"=>100,"content_type"=>"information"],
+    "keywordMapList"=>["fields"=>"content_id","source_type"=>"team","source_id"=>$team_id,"page_size"=>100,"content_type"=>"information","list"=>["page_size"=>6,"fields"=>"id,title,create_time"]],
     "informationList"=>["game"=>$config['game'],"page"=>1,"page_size"=>14,"type"=>"1,2,3,5"],
     "currentPage"=>["name"=>"team","id"=>$team_id,"site_id"=>$config['site_id']]
 
 ];
 $return = curl_post($config['api_get'],json_encode($data),1);
 $return['totalTeamInfo']['data']['race_stat'] = json_decode($return['totalTeamInfo']['data']['race_stat'],true);
-if(count($return["keywordMapList"]['data'])>0)
-{
-    $data2 = [
-        "informationList"=>["ids"=>array_column($return["keywordMapList"]['data'],"content_id"),"page_size"=>6,"fields"=>"id,title"]
-    ];
-    $return2 = curl_post($config['api_get'],json_encode($data2),1);
-    $connectedInformationList = $return2["informationList"]["data"];
-}
-else
+if(count($return["keywordMapList"]["data"])==0)
 {
     $data2 = [
         "informationList"=>["game"=>$config['game'],"page"=>1,"page_size"=>6,"type"=>"1,2,3,5"],
     ];
     $return2 = curl_post($config['api_get'],json_encode($data2),1);
     $connectedInformationList = $return2["informationList"]["data"];
+}
+else
+{
+    $connectedInformationList = $return["keywordMapList"]["data"];
 }
 ?>
 <head>
