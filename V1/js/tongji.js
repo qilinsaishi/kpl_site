a6dc362583e696afe70f5ffa87ad32e5f7e52eb0
xiaodong chen <cxd032404@hotmail.com>
2021-02-18 18:57:31 +0800

    merged

---
+++ V1/js/tongji.js
@@ -0,0 +1,8 @@
+// 百度统计
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?ef3acba567b4f102357416d86ba7c55e";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
