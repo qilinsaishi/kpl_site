b66bda5734fada1b05d33bff7a8bd34c94c95da5
xiaodong chen <cxd032404@hotmail.com>
2021-02-19 14:02:37 +0800

    merge

--- V1/function/config_master.php
+++ V1/function/config_master.php
@@ -38,6 +38,16 @@ $base_config = [
 
 $additional_config = [
     'site_description'=> $base_config['site_name'].'致力于服务广大'.$base_config['game_name'].'玩家，为'.$base_config['game_name'].'玩家提供丰富的'.$base_config['game_name'].'游戏攻略、'.$base_config['game_name'].'电子竞技赛事资讯、数据分析及内容解读。',
-    'api_get' => $base_config['api_url']."/get"
+    'api_get' => $base_config['api_url']."/get",
+    'api_sitemap' => $base_config['api_url']."/sitemap",
+    'navList' => ['index'=>['url'=>"","name"=>"首页"],
+        'game'=>['url'=>"gameint/","name"=>"游戏介绍"],
+        'team'=>['url'=>"teamlist/","name"=>"战队列表"],
+        'player'=>['url'=>"playerlist/","name"=>"选手列表"],
+        'hero'=>['url'=>"herolist/","name"=>"英雄介绍"],
+        'info'=>['url'=>"newslist/","name"=>"游戏资讯"],
+        'stra'=>['url'=>"strategylist/","name"=>"游戏攻略"],
+        //'faq'=>['url'=>"wenda-list.html","name"=>"游戏问答"],
+    ]
 ];
 return array_merge($base_config,$additional_config);
