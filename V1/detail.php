fb0f8d9b25e0dfb484570d8b02ffe39c2d41ef53
xiaodong chen <cxd032404@hotmail.com>
2021-02-24 14:27:30 +0800

    merge

--- V1/detail.php
+++ V1/detail.php
@@ -92,7 +92,7 @@
         <div class="nav">
             <ul>
                 <?php
-                $type = $return['information']['data']['type']==4 ?"info":"stra";
+                $type = $return['information']['data']['type']!=4 ?"info":"stra";
                 generateNav($config, $type);
                 ?>
             </ul>
